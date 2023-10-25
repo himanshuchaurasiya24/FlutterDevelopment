ef5ea7fe1ad9bd580ffd761771d3a12024468842
Himanshu Chaurasiya <himanshuchaurasiya24@gmail.com>
2023-10-25 17:32:50 +0530

    model_expense_cat_created

---
+++ expense_app/lib/screens/category_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CategoryScreen extends StatelessWidget {
+  const CategoryScreen({super.key});
+  static const name = '/category_screen';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Categories'),
+      ),
+    );
+  }
+}
